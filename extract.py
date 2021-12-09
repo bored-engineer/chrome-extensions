20b3dff247e3fb8cc6412fd2ad911265d3bf2e84
Luke Young <bored-engineer@users.noreply.github.com>
2021-12-08 16:50:44 -0800

    Update extract.py

--- extract.py
+++ extract.py
@@ -74,4 +74,5 @@ if __name__ == "__main__":
                             "size": traverse(data, 25),
                         }
                     ),
+                    indent=2,
                 )
